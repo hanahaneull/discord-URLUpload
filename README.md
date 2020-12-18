63815fa0abac87490db47532c28fc2378c46cb23
Haneul Seong <hana@disroot.org>
2020-12-18 17:35:08 +0700

    Format the code style
    Formated code style to JS Standard

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+
+#
+
 # Discord URL Uploader
 
 Upload Discord attachment straight from your discord account.
--- index.js
+++ index.js
@@ -1,93 +1,91 @@
-const config = require('./config.json');
-const mime = require('./mime.json');
-const got = require('got');
-const Discord = require('discord.js');
+const config = require('./config.json')
+const mime = require('./mime.json')
+const got = require('got')
+const Discord = require('discord.js')
 const client = new Discord.Client({
-	messageCacheMaxSize: 5,
-	messageCacheLifetime: 5,
-	messageSweepInterval: 5,
-	disabledEvents: [
-		'GUILD_UPDATE',
-		'GUILD_MEMBER_ADD',
-		'GUILD_MEMBER_REMOVE',
-		'GUILD_MEMBER_UPDATE',
-		'GUILD_MEMBERS_CHUNK',
-		'GUILD_ROLE_CREATE',
-		'GUILD_ROLE_DELETE',
-		'GUILD_ROLE_UPDATE',
-		'GUILD_BAN_ADD',
-		'GUILD_BAN_REMOVE',
-		'CHANNEL_UPDATE',
-		'CHANNEL_PINS_UPDATE',
-		'MESSAGE_DELETE',
-		'MESSAGE_UPDATE',
-		'MESSAGE_DELETE_BULK',
-		'MESSAGE_REACTION_ADD',
-		'MESSAGE_REACTION_REMOVE',
-		'MESSAGE_REACTION_REMOVE_ALL',
-		'USER_UPDATE',
-		'USER_NOTE_UPDATE',
-		'USER_SETTINGS_UPDATE',
-		'PRESENCE_UPDATE',
-		'VOICE_STATE_UPDATE',
-		'TYPING_START',
-		'VOICE_SERVER_UPDATE',
-		'RELATIONSHIP_ADD',
-		'RELATIONSHIP_REMOVE',
-	],
-	http: {
-		version: 8,
-		api: 'https://discord.com/api',
-	},
-});
+  messageCacheMaxSize: 5,
+  messageCacheLifetime: 5,
+  messageSweepInterval: 5,
+  disabledEvents: [
+    'GUILD_UPDATE',
+    'GUILD_MEMBER_ADD',
+    'GUILD_MEMBER_REMOVE',
+    'GUILD_MEMBER_UPDATE',
+    'GUILD_MEMBERS_CHUNK',
+    'GUILD_ROLE_CREATE',
+    'GUILD_ROLE_DELETE',
+    'GUILD_ROLE_UPDATE',
+    'GUILD_BAN_ADD',
+    'GUILD_BAN_REMOVE',
+    'CHANNEL_UPDATE',
+    'CHANNEL_PINS_UPDATE',
+    'MESSAGE_DELETE',
+    'MESSAGE_UPDATE',
+    'MESSAGE_DELETE_BULK',
+    'MESSAGE_REACTION_ADD',
+    'MESSAGE_REACTION_REMOVE',
+    'MESSAGE_REACTION_REMOVE_ALL',
+    'USER_UPDATE',
+    'USER_NOTE_UPDATE',
+    'USER_SETTINGS_UPDATE',
+    'PRESENCE_UPDATE',
+    'VOICE_STATE_UPDATE',
+    'TYPING_START',
+    'VOICE_SERVER_UPDATE',
+    'RELATIONSHIP_ADD',
+    'RELATIONSHIP_REMOVE'
+  ],
+  http: {
+    version: 8,
+    api: 'https://discord.com/api'
+  }
+})
 
 client.on('ready', () => {
-	console.log(`${client.user.tag} Logged in`);
-});
+  console.log(`${client.user.tag} Logged in`)
+})
 
 client.on('error', (err) => {
-	console.log(err.message);
-});
+  console.log(err.message)
+})
 
 client.on('warn', (warn) => {
-	console.log(warn);
-});
+  console.log(warn)
+})
 
 client.on('message', async (msg) => {
-	if (msg.author.id !== client.user.id) return;
-	const prefix = config.prefix;
-	const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-	const command = args.shift().toLowerCase();
+  if (msg.author.id !== client.user.id) return
+  const prefix = config.prefix
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
 
-	switch (command) {
-		case 'send':
-			await msg.delete();
-			(async () => {
-				try {
-					const response = await got(args[0], { responseType: 'buffer' });
-					const buffer = response.body;
-					const type =
-						mime[`${response.headers['content-type']}`.split(';')[0]];
+  switch (command) {
+    case 'send':
+      await msg.delete()(async () => {
+        try {
+          const response = await got(args[0], { responseType: 'buffer' })
+          const buffer = response.body
+          const type = mime[`${response.headers['content-type']}`.split('')[0]]
 
-					msg.channel
-						.send('', {
-							file: {
-								attachment: buffer,
-								name: `${Date.now()}.${type}`,
-							},
-						})
-						.catch((e) => {
-							console.log(`[DISCORD] ERROR: ${e.message}`);
-						});
-				} catch (error) {
-					console.log(`[URL] ERROR: ${error.code}`);
-				}
-			})();
-			break;
+          msg.channel
+            .send('', {
+              file: {
+                attachment: buffer,
+                name: `${Date.now()}.${type}`
+              }
+            })
+            .catch((e) => {
+              console.log(`[DISCORD] ERROR: ${e.message}`)
+            })
+        } catch (error) {
+          console.log(`[URL] ERROR: ${error.code}`)
+        }
+      })()
+      break
 
-		default:
-			break;
-	}
-});
+    default:
+      break
+  }
+})
 
-client.login(config.token);
+client.login(config.token)
